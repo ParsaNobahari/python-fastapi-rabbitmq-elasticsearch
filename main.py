4b6b5349774fd87da7542b7637b967f0465aace3
ParsaNobahari <parsa.nobahari@gmail.com>
2023-08-03 21:31:09 +0330

    rewriting consumer in a function, added fastapi fundamentals

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-#  import json
-#  from fastapi import FastAPI
-#  from elasticsearch_dsl import Search
-#  from elasticsearch import AsyncElasticsearch
-#
-#  app = FastAPI()
-#
-#  es = AsyncElasticsearch(
-#      "https://localhost:9000",
-#  )
-#
-#
-#  @app.post('/search')
-#  async def search(query: str):
-#      s = Search(using=es, index='my_index').query('match', name=query)
-#      response = await s.execute()
-#      return response.to_dict()
+# import json
+# from elasticsearch_dsl import Search
+
+# @app.post('/search')
+# async def search(query: str):
+#     s = Search(using=es, index='my_index').query('match', name=query)
+#     response = await s.execute()
+#     return response.to_dict()
+
 import os
 import sys
 import asyncio
 import aio_pika
+from fastapi import FastAPI
+from elasticsearch import AsyncElasticsearch
 
-num_of_async_proccesing_messages = 10
+num_of_async_processing_messages = 10
+
+app = FastAPI()
+
+es = AsyncElasticsearch(
+    "https://localhost:9000",
+)
+
+
+async def consume(queue):
+    async with queue.iterator() as queue_iter:
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        async for message in queue_iter:
+            async with message.process():
+                print(message.body.decode())
+
+                if queue.name in message.body.decode():
+                    break
 
 
 async def main() -> None:
@@ -29,22 +40,11 @@ async def main() -> None:
     )
 
     queue_name = "test_queue"
-
     async with connection:
         channel = await connection.channel()
-
-        await channel.set_qos(prefetch_count=num_of_async_proccesing_messages)
-
+        await channel.set_qos(prefetch_count=num_of_async_processing_messages)
         queue = await channel.declare_queue(queue_name, auto_delete=False)
-
-        async with queue.iterator() as queue_iter:
-            print(' [*] Waiting for messages. To exit press CTRL+C')
-            async for message in queue_iter:
-                async with message.process():
-                    print(message.body.decode())
-
-                    if queue.name in message.body.decode():
-                        break
+        await consume(queue)
 
 
 if __name__ == '__main__':
